d2db26f0cb410f42028e12bf74a5255723a9d149
Léa Hamadouche <leahamadouche@hotmail.com>
2021-10-29 09:05:19 +0200

    Dark Mode / Light Mode implemented
    Dark/Light Mode Toggler + Globalstyle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
-
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './components/themes/GlobalStyle';
+import { useDarkMode } from './components/themes/useDarkMode';
+import { lightTheme, darkTheme } from './components/themes/Themes';
+import Toggle from './components/themes/Toggler';
 function App() {
-  return <div></div>;
+  const [theme, themeToggler] = useDarkMode();
+
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+  return (
+    <ThemeProvider theme={themeMode}>
+      <div>
+        <GlobalStyles />
+        <Toggle theme={theme} toggleTheme={themeToggler} />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/themes/Toggler.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { func, string } from 'prop-types';
+import styled from 'styled-components';
+import srcLMIcon from './lightModeIcon.png';
+import srcDMIcon from './darkModeIcon.png';
+
+const Button = styled.button`
+  background: ${({ theme }) => theme.background};
+  border: 2px solid ${({ theme }) => theme.toggleBorder};
+  color: ${({ theme }) => theme.text};
+  border-radius: 10px;
+  cursor: pointer;
+  padding-top: 0.2rem;
+  }
+`;
+
+const LightModeIcon = styled.img`
+  height: auto;
+  width: 1.5rem;
+`;
+LightModeIcon.defaultProps = { src: srcLMIcon };
+
+const DarkModeIcon = styled.img`
+  height: auto;
+  width: 1.5rem;
+`;
+DarkModeIcon.defaultProps = { src: srcDMIcon };
+const Toggle = ({ theme, toggleTheme }) => {
+  return (
+    <>
+      <Button onClick={toggleTheme}>
+        {theme === 'light' ? (
+          <DarkModeIcon></DarkModeIcon>
+        ) : (
+          <LightModeIcon></LightModeIcon>
+        )}
+      </Button>
+    </>
+  );
+};
+Toggle.propTypes = {
+  theme: string.isRequired,
+  toggleTheme: func.isRequired,
+};
+export default Toggle;
