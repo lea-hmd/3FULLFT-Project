79b81dde9d49f98e56a42010b45a9b267412b4e1
Léa Hamadouche <leahamadouche@hotmail.com>
2021-10-31 18:05:06 +0100

    Removed  unused import

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import './App.css';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './components/themes/GlobalStyle';
-import { useDarkMode } from './components/themes/useDarkMode';
 import { lightTheme, darkTheme } from './components/themes/Themes';
-import Toggle from './components/themes/Toggler';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppStructure from './views/AppStructure';
 import { OrderContext } from '../src/context/OrderContext';
-import { Button } from '@mui/material';
-import DarkModeIcon from '../src/components/themes/Toggler';
-import LightModeIcon from '../src/components/themes/Toggler';
 function App() {
   //const [theme, themeToggler] = useDarkMode();
   const { orderState, orderDispatch } = React.useContext(OrderContext);
@@ -20,8 +15,6 @@ function App() {
       <ThemeProvider theme={themeMode}>
         <div>
           <GlobalStyles />
-
-          {/* <Toggle theme={theme} toggleTheme={themeToggler} /> */}
           <AppStructure></AppStructure>
         </div>
       </ThemeProvider>
